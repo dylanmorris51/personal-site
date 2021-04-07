a24c220f743747dbce7244fbe74e17fb0063b42d
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-07 13:41:07 -0500

    Added pictures, changed dynamic text, need to change name everywhere. I really hope this doesn't affect her code

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@
 
     $(function () {
         element.typed({
-            strings: ["Software Developer.", "Storyteller.", "Space Pirate."],
+            strings: ["Software Developer.", "Piano Player.", "Swing Dancer."],
             typeSpeed: 100,
             loop: true,
             autoplay: true,
